b4c8d54b40c6b18dce46a64881e1dbfe841dc8d1
Rohit72099 <112176838+Rohit72099@users.noreply.github.com>
2023-10-09 19:54:08 +0530

    Add files via upload

---
+++ opera_overload.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Number
+{
+int x;
+public:
+Number(int n)
+{
+x=n;
+}
+void operator++(int)
+{
+cout<<"\nPostfix increment:";
+cout<<x++;
+}
+friend void operator--(Number,int);
+};
+void operator--(Number obj1,int)
+{
+cout<<"\nPostfix decrement:";
+cout<<obj1.x--;
+}
+int main()
+{
+Number N1(10);
+//N1++;
+N1.operator++();//Member function//Any integer can be passed(no difference)
+//N1--;
+operator--(N1,100);//Friend function
+return 0;
+}
